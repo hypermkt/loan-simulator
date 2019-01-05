6b9d0062a21609f6efc85cdcd4452eff7c9d2b07
Makoto Chiba <hypermkt@gmail.com>
2019-01-06 00:57:43 +0900

    Add test cases

--- cmd/loan-simulator/main_test.go
+++ cmd/loan-simulator/main_test.go
@@ -4,6 +4,30 @@ import (
 	"testing"
 )
 
+func TestCalcRepaidAmount(t *testing.T) {
+	patterns := []struct {
+		p        Params
+		expected int
+	}{
+		{Params{
+			Year:                35,
+			Months:              420,
+			InterestRate:        1,
+			MonthlyInterestRate: 0.0008333333333333334,
+			AmountMan:           3000,
+			Amount:              30000000,
+			CurrentBalance:      30000000,
+		}, 84685},
+	}
+
+	for idx, pattern := range patterns {
+		actual := calcRepaidAmount(pattern.p)
+		if actual != pattern.expected {
+			t.Errorf("pattern %d: want %d, actual %d", idx, pattern.expected, actual)
+		}
+	}
+}
+
 func TestCalcMonths(t *testing.T) {
 	patterns := []struct {
 		y        int
@@ -38,6 +62,32 @@ func TestCalcInterest(t *testing.T) {
 		actual := calcInterest(p)
 		if actual != pattern.expected {
 			t.Errorf("pattern %d: want %d, actual %d", idx, pattern.expected, actual)
+		}
+	}
+}
+
+func TestToParams(t *testing.T) {
+	patterns := []struct {
+		y        int
+		ir       float64
+		a        int
+		expected Params
+	}{
+		{35, 1, 3500, Params{
+			Year:                35,
+			Months:              420,
+			InterestRate:        1,
+			MonthlyInterestRate: 0.0008333333333333334,
+			AmountMan:           3500,
+			Amount:              35000000,
+			CurrentBalance:      35000000,
+		}},
+	}
+
+	for idx, pattern := range patterns {
+		actual := toParams(pattern.y, pattern.ir, pattern.a)
+		if actual != pattern.expected {
+			t.Errorf("pattern %d: want %v, actual %v", idx, pattern.expected, actual)
 		}
 	}
 }
